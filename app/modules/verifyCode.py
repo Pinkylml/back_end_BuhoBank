c07b1bd91784a8edb34e8b898316be0966a7e2a4
Jefferson Cando <jeff_can1995@hotmail.es>
2024-07-18 16:07:34 -0500

    Eliminar registro luego de si pasa la verificacion

--- app/modules/verifyCode.py
+++ app/modules/verifyCode.py
@@ -12,14 +12,31 @@ async def getData(email):
         return False
     else:
         return result_data
+    
+
+async def deleteData(email):
+    query={
+        "email":email        
+    }
+    
+    result_data=await code_verify_collection.delete_one(query)
+    if result_data.deleted_count == 1:
+        return True
+    else:
+        return False
 
 async def verifyCodeFunction (data:verifyCode)->dict:
     data_flag=await getData(data.email)
+    print("En verificar el codigo",data_flag)
     if data_flag:
         save_code=data_flag['code']
         get_code=data.codigo
         get_code=int(get_code)
         if save_code==get_code:
-            return 200, {"code":"SUCCESS"}
+            delete_flag=await deleteData(data.email)
+            if delete_flag:
+                return 200, {"code":"SUCCESS"}
+            else:
+                return 200, {"code":"DELETE_NO_SUCCESS"}
         else:
             return 200, {"code":"NO_SUCCESS"}
