880a5e45a3e607b81324c726019f622ecf0a441d
Redjean <jeanasencio2000@gmail.com>
2024-07-18 20:27:07 -0500

    End-point para enviar codigo para resetear contraseña

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import ConnectionFailure
 
-MONGO_DETAILS = "mongodb+srv://jeffcan1995:zRlRxsF5sRbZiL5O@buhobankcluster.ei1v8qq.mongodb.net/?retryWrites=true&w=majority&appName=BuhoBankCluster"
+MONGO_DETAILS = "mongodb+srv://jeanasencio2000:passjean20001207@buhobanco.t77tcy2.mongodb.net/?retryWrites=true&w=majority&appName=BuhoBanco"
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
 
@@ -15,3 +15,4 @@ database = client.BuhoBanco
 customer_collection = database.get_collection("Clientes")
 account_collection = database.get_collection("CuentasBancarias")
 code_verify_collection= database.get_collection("CodigosVerificación")
+reset_verify_colletion= database.get_collection("CodigosVerificaciónReseteo")
--- app/modules/send_email.py
+++ app/modules/send_email.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import parseaddr
 import dns.resolver
-from ..database import customer_collection,account_collection,code_verify_collection
+from ..database import customer_collection,code_verify_collection, reset_verify_colletion
 from fastapi import FastAPI, HTTPException, Depends
 from datetime import datetime
 from ..models import CustomerModel
@@ -34,40 +34,74 @@ def send_email(subject, html_body, sender, recipients, password):
         return 400, {"code":"EMAIL_DONT_EXIST"}
     
 
-async def save(code,email):
+async def save(code,email,collection):
     query = {
         "email": email,
         "code":code,
         "attempts":3
     }
-    insert_result=await code_verify_collection.insert_one(query)
+    insert_result=await collection.insert_one(query)
     if insert_result.inserted_id:
         return True
     else:
         return False
-    
-async def prepare_email(email):
-    code=random.randint(100000, 999999)
-    subject = "Código de verificación"
-    html_body=f"""
-        <html>
-            <body>
-                <p>Su código de verificación es:</p>
-                <p><strong>{code}</strong></p>
-            </body>
-        </html>
-        """
-    sender = "buhobanco@gmail.com"
-    recipients = [f"{email}"]
-    password =os.getenv('SMTP_APP_PASSWORD_GOOGLE')
-    save_flag=await save(code,email)
-    if save_flag:
-        status,response=send_email(subject, html_body, sender, recipients, password)
-    else:
-        status=200
-        response={"code":"DONT_SAVE_CODE"}
 
-    return status,response
+
+async def prepare_email(email, parametro):
+    #0 Para registro
+    #1 para el reseteo
+    code=random.randint(100000, 999999)
+    save_flag = ""
+    if parametro == 0:
+        save_flag=await save(code,email, code_verify_collection)
+        subject = "Código de verificación"
+        html_body=f"""
+            <html>
+                <body>
+                    <p>Su código de verificación es:</p>
+                    <p><strong>{code}</strong></p>
+                </body>
+            </html>
+            """
+        sender = "buhobanco@gmail.com"
+        recipients = [f"{email}"]
+        password =os.getenv('SMTP_APP_PASSWORD_GOOGLE')
+        if save_flag:
+            status,response=send_email(subject, html_body, sender, recipients, password)
+            return status,response
+        else:
+            status=200
+            response={"code":"DONT_SAVE_CODE"}
+            return status,response
+    elif parametro==1:
+        check = await CheckIsRegistered(code, email)
+        if check:
+            saveResult = await save(code, email, reset_verify_colletion)
+            if saveResult:
+                subject = "Código de verificación"
+                html_body=f"""
+                    <html>
+                        <body>
+                            <h1>Código para resetear contraseña</h1>
+                            <p>Su código de verificación es:</p>
+                            <p><strong>{code}</strong></p>
+                        </body>
+                    </html>
+                    """
+                sender = "buhobanco@gmail.com"
+                recipients = [f"{email}"]
+                password =os.getenv('SMTP_APP_PASSWORD_GOOGLE')
+                status,response=send_email(subject, html_body, sender, recipients, password)
+                return status, response
+            else:
+                status=200
+                response={"code":"DONT_SAVE_CODE"}
+                return status,response
+        else:
+            status = 200
+            response={"code":"EMAIL_DONT_EXIST"}
+            return status, response
+        
     
 async def preVerifyToSendEmail(customer: CustomerModel):
     ci,credentials=await verifyDataCI(customer)
@@ -84,9 +118,17 @@ async def preVerifyToSendEmail(customer: CustomerModel):
                 response = {"code": "EMAIL_REPEAT"}
                 return 200,response
             else:
-                status,response=await prepare_email(customer.email)
+                status,response=await prepare_email(customer.email, 0)
                 return status,response
 
+async def CheckIsRegistered(code, email):
+    result = await customer_collection.find_one({"email": email})
+    print(result, "CheckIsRegistered")
+    if result:
+        return True
+    else:
+        print("No hay cuenta para agregar cod.")
+        return False
 
 
 
